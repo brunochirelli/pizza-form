b148f6d9becfa870c8194f5595db313fe87ca18a
Bruno <brunochirelli@gmail.com>
2021-05-20 18:20:24 -0300

    add order slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import orderReducer from "../features/order/orderSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    order: orderReducer,
   },
 });
 
