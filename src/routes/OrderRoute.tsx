458bd7216b85eaedc7314c65a932b208ec9a47ad
Bruno <brunochirelli@gmail.com>
2021-05-20 18:18:16 -0300

    add hoc order route

---
+++ src/routes/OrderRoute.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Redirect, Route, RouteProps } from "react-router";
+import { useAppSelector } from "../app/hooks";
+
+interface OrderRouteProps extends RouteProps {
+  /**
+   * Need help to type it better
+   */
+  component?: any;
+}
+
+/**
+ * Higher-Order-Component to prevent advance steps without the base topping has
+ * been chosen.
+ *
+ * @version 0.0.1
+ * @returns HOC wrapping Route
+ */
+const OrderRoute = ({ component: Component, ...rest }: OrderRouteProps) => {
+  const { pizza } = useAppSelector((state) => state.order.order);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (!pizza.name) {
+          return <Redirect to="/pedido/recheio" />;
+        }
+        return <Component {...props} />;
+      }}
+    />
+  );
+};
+
+export default OrderRoute;
